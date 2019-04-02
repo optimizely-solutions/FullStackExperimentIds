39a0f0071181c42e1bd7cb9b9c82b5d37d2c1bdf
Thilo Richter <trichter@ams-trichter1.local>
2019-04-02 11:16:17 +0200

    added csv copy

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-console.log("Fullstack IDs v0.2.2");
+console.log("Fullstack IDs v0.2.3");
+var csv = "";
+
 var optly = new OptimizelyAPI({
     password: 'fsexperimentids',
     oauth_client_id: 14330300653
   });
+
+function download_csv() {
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'experiments_by_id.csv';
+    hiddenElement.click();
+}
+
 
 var form = document.getElementById("simple-form")
 form.onsubmit = function (event) { 
@@ -11,13 +22,14 @@ form.onsubmit = function (event) {
     optly.get("experiments?per_page=100&project_id=" + projectID, function(exps){
         console.log(exps);
         var htmlString = "";
-        var csvString = "";
+        var button = "<button onclick='download_csv()'>Download CSV</button>";
         for (let i = 0; i < exps.length; i++) {
             var experiment = exps[i];
             htmlString += "<p>" + experiment.id + " ----> " + experiment.key + "</p>";
-            csvString += experiment.id + "," + experiment.key + "\n";
+            csv += experiment.id + "," + experiment.key + "\n";
         }
         document.getElementById("experiments").innerHTML = htmlString;
-        document.getElementById("csv").innerHTML = csvString;
+        document.getElementById("download").innerHTML = button;
     });
 }
+
