edff546b824426f5b076f951ed1db6b849cf91ea
Thilo Richter <trichter@ams-trichter1.local>
2019-04-02 11:09:00 +0200

    added csv copy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Fullstack IDs v0.2.1");
+console.log("Fullstack IDs v0.2.2");
 var optly = new OptimizelyAPI({
     password: 'fsexperimentids',
     oauth_client_id: 14330300653
@@ -11,10 +11,13 @@ form.onsubmit = function (event) {
     optly.get("experiments?per_page=100&project_id=" + projectID, function(exps){
         console.log(exps);
         var htmlString = "";
+        var csvString = "";
         for (let i = 0; i < exps.length; i++) {
             var experiment = exps[i];
-            htmlString += "<p>" + experiment.id + " , " + experiment.key + "</p>";
+            htmlString += "<p>" + experiment.id + " ----> " + experiment.key + "</p>";
+            csvString += experiment.id + "," + experiment.key + "\n";
         }
         document.getElementById("experiments").innerHTML = htmlString;
+        document.getElementById("csv").innerHTML = csvString;
     });
 }
