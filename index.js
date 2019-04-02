6273fc83c1b80d3282b6a06433dcd6720511c03b
Thilo Richter <trichter@ams-trichter1.local>
2019-04-02 10:56:48 +0200

    add jquery

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("v0.1.3");
+console.log("v0.1.31");
 var optly = new OptimizelyAPI({
     password: 'fsexperimentids',
     oauth_client_id: 14330300653
@@ -8,7 +8,7 @@ var form = document.getElementById("simple-form")
 form.onsubmit = function (event) { 
     event.preventDefault();
     var projectID = document.getElementById("projectid").value;
-    optly.get("experiments?project_id=" + value, function(exps){
+    optly.get("experiments?project_id=" + projectID, function(exps){
         console.log(exps);
     });
 }
