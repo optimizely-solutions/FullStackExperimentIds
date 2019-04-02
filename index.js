003d8522a8905bbed292c0e0ad87006ac71bff7e
Thilo Richter <trichter@ams-trichter1.local>
2019-04-02 10:47:22 +0200

    change client

---
+++ index.js
@@ -0,0 +1,12 @@
+var optly = new OptimizelyAPI({
+    password: 'fsexperimentids',
+    oauth_client_id: 14330300653
+  });
+
+var form = document.getElementById("simple-form")
+form.onsubmit = function (event) { 
+    event.preventDefault(); 
+    optly.get("experiments?project_id=13239170233", function(exps){
+        console.log(exps);
+    });
+}
